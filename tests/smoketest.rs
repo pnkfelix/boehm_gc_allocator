36f4683556de29979839728d7186dc8590c140b2
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-11-07 13:29:52 +0100

    Added manual registration call to fix the smoketest, as well as comments documenting the other avenues I considered taking and why they don't currently work out.

--- tests/smoketest.rs
+++ tests/smoketest.rs
@@ -13,8 +13,24 @@ macro_rules! test {
     }
 }
 fn main() {
+    // This does not work with DARWIN_DONT_PARSE_STACK, which seems to be turned on
+    // by default (despite what the documentation implies).
+    //
     // boehm_gc::use_threads_discovery();
+
+    // This works but is unnnecessary with our own private build of libgc, which
+    // has GC_ALWAYS_MULTITHREADED turned on when it is built.
+    //
     // unsafe { boehm_gc::gc_allow_register_threads(); }
+
+    // This is probably what we want to encourage others to do, but it requires
+    // std::thread::at_start, which is not part of Rust.
+    // I started a discussion of this at:
+    //   https://internals.rust-lang.org/t/rfrfc-std-thread-at-start-callback/2877
+    // but who knows if it will go anywhere.
+    // For now, you can emulate the effect of this by manually invoking `gc_register_myself`
+    // at the start of each newly spawned thread.
+    //
     // ::std::thread::at_start(|| { unsafe { boehm_gc::gc_register_myself(); } });
 
     let tests = vec![test!(hello_gc_alloc)];
@@ -24,6 +40,7 @@ fn main() {
 
 fn hello_gc_alloc() {
     use boehm_gc::{gc_allocate};
+    unsafe { boehm_gc::gc_register_myself(); }
     const BYTES: usize = 408;
     println!("Hello gc_allocate {} bytes", BYTES);
     for _i in 0..1000 {
