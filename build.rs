3db8530ff02b04d504ce242b5c3847c23a1242cc
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-11-09 17:02:53 +0100

    added instrumentation output from build script to stderr.

--- build.rs
+++ build.rs
@@ -3,10 +3,18 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
 
+macro_rules! printerr {
+    ($($expr:expr),*) => { writeln!(&mut std::io::stderr(), $($expr),*) }
+}
+
 fn main() {
-    let current_dir = env::current_dir().unwrap();
+    let current_dir = env::current_dir();
+    if current_dir.is_err() { printerr!("build no current_dir"); }
+    let current_dir = current_dir.unwrap();
     // let target_dir = os::get_env("CARGO_TARGET_DIR").unwrap();
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR");
+    if out_dir.is_err() { printerr!("build no OUT_DIR"); }
+    let out_dir = out_dir.unwrap();
     let mut bdw_dir = current_dir.clone();
     bdw_dir.push("src");
     bdw_dir.push("bdwgc");
@@ -17,11 +25,13 @@ fn main() {
 
     let mut cmd = Command::new(configure_path);
     cmd.current_dir(out_dir.clone());
-    let cmd_output = cmd.output().unwrap();
+    let cmd_output = cmd.output();
+    if cmd_output.is_err() { printerr!("build no cmd_output"); }
+    let cmd_output = cmd_output.unwrap();
     if !cmd_output.status.success() {
-        writeln!(&mut std::io::stderr(), "configure status: {}", cmd_output.status);
-        writeln!(&mut std::io::stderr(), "configure stdout: {}", String::from_utf8_lossy(&cmd_output.stdout));
-        writeln!(&mut std::io::stderr(), "configure stderr: {}", String::from_utf8_lossy(&cmd_output.stderr));
+        printerr!("configure status: {}", cmd_output.status);
+        printerr!("configure stdout: {}", String::from_utf8_lossy(&cmd_output.stdout));
+        printerr!("configure stderr: {}", String::from_utf8_lossy(&cmd_output.stderr));
     }
     assert!(cmd_output.status.success());
 
@@ -36,19 +46,25 @@ fn main() {
     let mut cmd = Command::new("make");
     cmd.current_dir(out_dir.clone());
     cmd.arg(make_cflags);
-    let cmd_output = cmd.output().unwrap();
+    let cmd_output = cmd.output();
+    if cmd_output.is_err() { printerr!("build make failed"); }
+    let cmd_output = cmd_output.unwrap();
     if !cmd_output.status.success() {
-        writeln!(&mut std::io::stderr(), "make status: {}", cmd_output.status);
-        writeln!(&mut std::io::stderr(), "make stdout: {}", String::from_utf8_lossy(&cmd_output.stdout));
-        writeln!(&mut std::io::stderr(), "make stderr: {}", String::from_utf8_lossy(&cmd_output.stderr));
+        printerr!("make status: {}", cmd_output.status);
+        printerr!("make stdout: {}", String::from_utf8_lossy(&cmd_output.stdout));
+        printerr!("make stderr: {}", String::from_utf8_lossy(&cmd_output.stderr));
     }
+
     assert!(cmd_output.status.success());
 
     let mut libs_dir = PathBuf::from(out_dir);
     libs_dir.push(".libs");
     let mut libgc_a = libs_dir.clone();
     libgc_a.push("libgc.a");
-    assert!(fs::metadata(libgc_a).unwrap().is_file());
+    let libgc_a_metadata = fs::metadata(libgc_a);
+    if libgc_a_metadata.is_err() { printerr!("build make libgc.a unfound."); }
+    let libgc_a_metadata = libgc_a_metadata.unwrap();
+    assert!(libgc_a_metadata.is_file());
 
     println!("cargo:rustc-link-lib=static=gc");
     println!("cargo:rustc-link-search=native={}", libs_dir.display());
